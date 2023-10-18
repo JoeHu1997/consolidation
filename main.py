fab08ab8a536503a0e2387f1ef2fee4c7be0820f
JoeMac <aszx11112@gmail.com>
2023-10-18 20:31:08 +0800

    建立讀取函數，，會根據輸入的json改變屬性

---
+++ main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+from numpy import empty
+# Load consolidation data from a JSON file
+
+class MatrixCalculator:
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = None
+        self.calculate_matrix()
+
+    def load_data(self):
+        with open(self.filename, 'r') as json_file:
+            data = json.load(json_file)
+            #print(data)
+            for key, value in data.items():
+                setattr(self, key, value)
+            
+
+    def calculate_matrix(self):
+        if self.data is None:
+            self.load_data()
+        '''
+        a_matrix = np.array(self.rainfall)
+        b_matrix = np.array(self.runoff)
+        
+        # Perform matrix calculations here
+        result = np.dot(a_matrix, b_matrix)
+        
+        return result
+        '''
+
+if __name__ == "__main__":
+    calculator = MatrixCalculator('consolidation_data.json')
+    print(calculator.type)
