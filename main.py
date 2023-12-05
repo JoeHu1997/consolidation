894414f06ecefb62d527e4ccbbd74033ad75d004
Joe Hu <Joe at Home>
2023-12-05 20:26:20 +0800

    與孔隙有關的參數建立 尚未完成

--- main.py
+++ main.py
@@ -8,23 +8,34 @@ class MatrixCalculator:
     def __init__(self, filename):
         self.filename = filename
         self.data = None
-        self.calculate_matrix()
+        self.load_data()
+        self.calculate_pore()
 
     def load_data(self):
         with open(self.filename, 'r') as json_file:
             data = json.load(json_file)
-            #print(data)
             for key, value in data.items():
                 setattr(self, key, value)
     
-    def calculate_gamma(self,e=None,):
+    def calculate_pore(self):
+        parameters ={'e', 'Gs', 'w', 'S'}
+        provided_params =vars(self)
+        missing_params = [param for param in parameters if param not in provided_params]
+        print(provided_params)
+        print(parameters)
+        print(missing_params)
+    #def calculate_gamma(self,e=None,):
+
 
             
-
+    '''
     def calculate_matrix(self):
         if self.data is None:
             self.load_data()
-        '''
+    '''
+    
+            
+    '''
         a_matrix = np.array(self.rainfall)
         b_matrix = np.array(self.runoff)
         
@@ -36,4 +47,4 @@ class MatrixCalculator:
 
 if __name__ == "__main__":
     calculator = MatrixCalculator('consolidation_data.json')
-    print(calculator.type)
+    #print(calculator.firstSoil)
