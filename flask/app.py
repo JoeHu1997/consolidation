f1d9bcc4d35c448e0bec23fa35487be0c20ca780
Joe Hu <Joe at Home>
2023-12-07 23:41:38 +0800

    最簡易模板做好了

--- flask/app.py
+++ flask/app.py
@@ -11,21 +11,45 @@ def input():
 
 @app.route("/polecalculater", methods=["POST"])
 def polecalculater():
-    Gs = request.form.get("item1")
-    w = request.form.get("item2")
-    S = request.form.get("item3")
-    e = request.form.get("item4")
+    def convert_to_int(value_str):
+        try:
+            return int(value_str) if value_str is not None else None
+        except ValueError:
+            return None
+    
+    Gs_str = request.form.get("item1")
+    w_str = request.form.get("item2")
+    S_str = request.form.get("item3")
+    e_str = request.form.get("item4")
+
+    Gs = convert_to_int(Gs_str)
+    w = convert_to_int(w_str)
+    S = convert_to_int(S_str)
+    e = convert_to_int(e_str)
+    
+    # 定義參數集合
     parameters = [e, Gs, w, S]
+
     # 定義提供的參數集合
     provided_params = ['e', 'Gs', 'w', 'S']
-    # 檢查每個值是否為空
+
+
     for param_name, param_value in zip(provided_params, parameters):
         if param_value is None or param_value == "":
-            if param_name == 
-            print(f"Error: {param_name} is empty.")
-    print(parameters)
-    
-    return "work"
+            if param_name == 'e':
+                e = Gs*w/S
+                print(1)
+            elif param_name == 'Gs':
+                Gs = S*e/w
+                print(2)
+            elif param_name == 'w':
+                w = S*e/Gs
+                print(3)
+            else:
+                S = Gs*w/e
+                print(4)
+    print(parameters)    
+    return render_template("output.html", item1=Gs, item2=w, item3=S, item4=e)
 @app.route("/head")
 def index():
     '''
