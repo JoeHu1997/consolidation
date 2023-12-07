69a6568458958599c4b84af06f35b9913ae735fb
Joe Hu <Joe at Home>
2023-12-08 00:07:38 +0800

    刪掉多餘的東西

--- flask/app.py
+++ flask/app.py
@@ -50,40 +50,6 @@ def polecalculater():
                 print(4)
     print(parameters)    
     return render_template("output.html", item1=Gs, item2=w, item3=S, item4=e)
-@app.route("/head")
-def index():
-    '''
-    print("請求方法:", request.method)   #(物件.屬性)
-    print("通訊協定 :", request.scheme)   
-    print("主機名稱 :", request.host)
-    print("路徑 :",request.path)
-    print("完整的網址 :",request.url)
-    '''
-    print("瀏覽器何作業系統 :", request.headers.get("user-agent")) 
-    print("語言偏好 :", request.headers.get("accept-language"))
-    print("引薦網址", request.headers.get("referrer"))
-    return "Hello Flask"
 
-@app.route("/posttest",methods=['POST'])
-def home2():
-    print(request.form)            # 使用 request.form
-    return "<h1>hello world</h1>"
-
-
-#get的路由模板
-@app.route('/data/appInfo/<name>', methods=['GET'])
-def queryDataMessageByName(name):
-    print("type(name) : ", type(name))
-    return 'String => {}'.format(name)
-
-@app.route('/data/record',methods=['GET','POST'])
-def login():
-    #  利用request取得使用者端傳來的方法為何
-    if request.method == 'POST':
-                          #  利用request取得表單欄位值
-        return 'Hello ' + request.values['username']
-    
-    #  非POST的時候就會回傳一個空白的模板
-    return render_template('login.html')
 app.debug = True
 app.run(port=5000)
